cbdbc3ca9011163e144eac2b61555b7e07d0bff9
Larasatiindaryani <88530090+Larasatiindaryani@users.noreply.github.com>
2021-08-20 16:42:36 +0700

    Latihan4
    Selesai

---
+++ Script/Karakter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Karakter {
+	private string name ;
+	private int health ;
+	private int damage;
+
+	public string Name {
+		get {
+			return name;
+		}
+		set {
+			name = value;
+		}
+	}
+
+	public int Health {
+		get {
+			return health;
+		}
+		set 
+		{
+			health = value;
+		}
+	}
+		public int Damage
+		{
+			get
+			{
+				return damage;
+			}
+			set
+			{
+				damage = value;
+			}
+	}
+		public void Moveleft()
+		{
+			Debug.Log("Gerak ke kiri");
+		}
+		public void MoveTRight()
+		{
+			Debug.Log("Gerak KeyCode kanan");
+		}
+		public void Jump()
+		{
+			Debug.Log("Loncat");
+		}
+		public void Attack()
+	{
+		Debug.Log ("Serang");
+	}
+}
