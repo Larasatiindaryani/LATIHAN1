8ee949e707a3707a38cedb893d883bf4255b8492
Larasatiindaryani <88530090+Larasatiindaryani@users.noreply.github.com>
2021-08-20 16:54:36 +0700

    Latihan3
    Selesai

---
+++ Latihan3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int c = jumlah(60, 40);
+		Debug.Log ("Hasil Jumlah a dan b adalah " + c);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	/// <summary>
+	/// Merupakan fungsi penjumlahan dari 2 input angka
+	/// </summary>
+	/// <param name="a">The alpha component.</param>
+	/// <param name="b">The blue component.</param>
+	int jumlah(int a, int b)
+	{
+		return a + b;
+	}
+}
